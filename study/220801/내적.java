65f23b26b99affb9abd6fd917d14c0cd885f78b0
DESKTOP-ALQAOR0\jiiiiiyoon <wldbs5897@naver.com>
2022-08-01 11:38:38 +0900

    Pro.내적_complete

---
+++ study/220801/내적.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solve() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int[] a = {-1, 0, 1};
+            int[] b = {1, 0, -1};
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+        new Main().solve();
+    }
+}
